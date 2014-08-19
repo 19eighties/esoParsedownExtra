a7f28600c051b50340d97d2094e8f81ecf0eacfc
kassius <kassiv.s@gmail.com>
2014-08-19 06:17:46 -0300

    initial

--- README.md
+++ README.md
@@ -1,4 +1,43 @@
 esoParsedownExtra
-=================
+=====================
 
-Markdown Extra syntax using Parsedown + ParsedownExtra for esoTalk
+Parsedown Markdown Extra plugin for esoTalk
+It is working but was not tested with other plugins, feedback is welcome.
+
+## Installation
+
+### 1. First method, getting the master .zip file
+
+1. Download the file https://github.com/kassius/esoParsedownExtra/archive/master.zip
+2. Extract it to your [esoTalk instalation directory]/addons/plugins/
+  * Then you should have the directory [your root install]/addons/plugins/esoParsedownExtra/
+4. Go to 'administration' on the site, then to 'Plugins' and enable 'esoMarkdownExtra' plugin.
+5. Then it should be working. Just create a post using Markdown syntax and test it.
+
+### 2. Second method, cloning this repository to your plugins/ directory via command line
+
+Via command line, commands are:
+
+~~~bash
+cd esoTalk/addons/plugins # go to plugins directory inside your esoTalk installation
+git clone https://github.com/kassius/esoParsedownExtra # clone the repository
+rm -r esoParsedownExtra/.git # delete git files for safety, unless you want to update it later via command line, then restrict access to this directory in your server's configuration
+~~~
+
+Then go to forum administration, and enable the plugin.
+
+## Reference
+* Parsedown
+  * http://parsedown.org/
+  * https://github.com/erusev/parsedown
+  * https://github.com/erusev/parsedown-extra
+* Markdown
+  * http://daringfireball.net/projects/markdown/
+  * http://en.wikipedia.org/wiki/Markdown
+* Markdown Extra
+  * https://michelf.ca/projects/php-markdown/extra/
+  * https://github.com/michelf/php-markdown/
+  * https://michelf.ca/projects/php-markdown/configuration/
+* esoTalk Forum Software
+  * http://esotalk.org/forum/
+  * http://esotalk.org/
---
+++ plugin.php
@@ -0,0 +1,88 @@
+<?php
+
+if (!defined("IN_ESOTALK")) exit;
+
+ET::$pluginInfo["esoParsedownExtra"] = array(
+    "name"        => "esoParsedownExtra",
+    "description" => "This plugin uses Parsedown + ParsedownExtra libraries to render text (http://parsedown.org/).",
+    "version"     => "1.0",
+    "author"      => "Kassius Iakxos",
+    "authorEmail" => "kassius@users.noreply.github.com",
+    "authorURL"   => "http://github.com/kassius",
+    "license"     => "GPLv2"
+);
+
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT);
+
+require_once(PATH_PLUGINS."/esoParsedownExtra/Parsedown.php");
+require_once(PATH_PLUGINS."/esoParsedownExtra/ParsedownExtra.php");
+require_once(PATH_CORE."/lib/ETFormat.class.php");
+
+class PDETFormat extends ETFormat
+{
+	public function format()
+	{
+		if (C("esoTalk.format.mentions")) $this->mentions();
+		if (!$this->inline) $this->quotes();
+		$this->closeTags();
+		
+		return $this;
+	}
+
+	public function links()
+	{
+		// Convert normal links - http://www.example.com, www.example.com - using a callback function.
+		$this->content = preg_replace_callback(
+			"/(?<=\s|^|>|&lt;)(\w+:\/\/)?([\w\-\.]+\.(?:AC|AD|AE|AERO|AF|AG|AI|AL|AM|AN|AO|AQ|AR|ARPA|AS|ASIA|AT|AU|AW|AX|AZ|BA|BB|BD|BE|BF|BG|BH|BI|BIZ|BJ|BM|BN|BO|BR|BS|BT|BV|BW|BY|BZ|CA|CAT|CC|CD|CF|CG|CH|CI|CK|CL|CM|CN|CO|COM|COOP|CR|CU|CV|CW|CX|CY|CZ|DE|DJ|DK|DM|DO|DZ|EC|EDU|EE|EG|ER|ES|ET|EU|FI|FJ|FK|FM|FO|FR|GA|GB|GD|GE|GF|GG|GH|GI|GL|GM|GN|GOV|GP|GQ|GR|GS|GT|GU|GW|GY|HK|HM|HN|HR|HT|HU|ID|IE|IL|IM|IN|INFO|INT|IO|IQ|IR|IS|IT|JE|JM|JO|JOBS|JP|KE|KG|KH|KI|KM|KN|KP|KR|KW|KY|KZ|LA|LB|LC|LI|LK|LR|LS|LT|LU|LV|LY|MA|MC|MD|ME|MG|MH|MIL|MK|ML|MM|MN|MO|MOBI|MP|MQ|MR|MS|MT|MU|MUSEUM|MV|MW|MX|MY|MZ|NA|NAME|NC|NE|NET|NF|NG|NI|NL|NO|NP|NR|NU|NZ|OM|ORG|PA|PE|PF|PG|PH|PK|PL|PM|PN|POST|PR|PRO|PS|PT|PW|PY|QA|RE|RO|RS|RU|RW|SA|SB|SC|SD|SE|SG|SH|SI|SJ|SK|SL|SM|SN|SO|SR|ST|SU|SV|SX|SY|SZ|TC|TD|TEL|TF|TG|TH|TJ|TK|TL|TM|TN|TO|TP|TR|TRAVEL|TT|TV|TW|TZ|UA|UG|UK|US|UY|UZ|VA|VC|VE|VG|VI|VN|VU|WF|WS|XXX|YE|YT|ZA|ZM|ZW)(?:[\.\/#][^\s<]*?)?)(?=\)\s|[\s\.,?!>]*(?:\s|&gt;|>|$))/i",
+			array($this, "linksCallback"), $this->content);
+
+		// Convert email links.
+		$this->content = preg_replace("/[\w-\.]+@([\w-]+\.)+[\w-]{2,4}/i", "<a href='mailto:$0' class='link-email'>$0</a>", $this->content);
+
+		return $this;
+	}
+	
+}
+
+class ETPlugin_esoParsedownExtra extends ETPlugin
+{
+	public $content;
+	public $PDETFormat;
+	public $PDEParser;
+
+	public function init()
+	{
+		$this->PDETFormat = new PDETFormat;
+		$this->PDEParser = new ParsedownExtra;
+	}
+
+	public function handler_format_beforeFormat($sender)
+	{
+		$this->PDETFormat->content = $sender->get();
+	}
+
+	public function handler_format_afterFormat($sender)
+	{
+		$this->PDETFormat->links(); 
+		
+		$search = array("\r&gt; ","\n&gt; ");
+		$this->PDETFormat->content = str_replace($search, "\n> ", $this->PDETFormat->content);
+		$this->PDETFormat->content = $this->PDEParser->text($this->PDETFormat->content);
+
+		$this->PDETFormat->content = str_replace("\r", "\n", $this->PDETFormat->content);
+		while(strstr($this->PDETFormat->content,"\n\n") !== FALSE) { $this->PDETFormat->content = str_replace("\n\n", "", $this->PDETFormat->content); }
+
+		$this->PDETFormat->format();
+		$this->PDETFormat->content = str_replace("\\\"", "\"", $this->PDETFormat->content);
+
+		$sender->content = $this->PDETFormat->content;
+	}
+
+	public function handler_conversationController_renderBefore($sender)
+	{
+		$sender->addCSSFile($this->resource("markdown.css"));
+	}
+
+}
+
+?>
